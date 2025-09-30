2f4e436ab2316e6cf2129611acab2d137124c0c4
jmmaa <reviuy9@gmail.com>
2025-09-30 21:36:47 +0800

    feat: intro for new command handler

--- modules/cmd/client.py
+++ modules/cmd/client.py
@@ -1,78 +1,142 @@
 from __future__ import annotations
+import asyncio
 from dataclasses import dataclass
 import typing as t
 import logging
 
-from dew import CommandNode, KwargNodes, get_keyword_seq, get_kwargs
+from dew import CommandNode, get_keyword_seq
+import dew
 
-from modules.command.di import DependencyInjector
+from yui.utils import maybe_await
 
+from pprint import pprint
 
 logging = logging.getLogger(__name__)
+
+
+# TODO
+
+
+# command_client = CommandClient({})
+
+
+# @command_client.command
+# @t.overload
+# def main():
+#     return 0
+
+
+# @main.command
+# def main():
+#     return 0
+
+
+# async def _main():
+#     print(command_client.registry)
+#     print(await command_client.execute("main"))
+
+
+# asyncio.run(_main())
 
 
 @dataclass
 class Command:
     name: str
     func: t.Callable[..., t.Any | None]
-    registry: CommandRegistry
-
-    def command(self, func: t.Callable[..., t.Any | None]):
-        self.registry[func.__name__] = Command(func.__name__, func, {})
-
-        return self.registry[func.__name__]
-
-
-CommandRegistry: t.TypeAlias = dict[str, Command]
 
 
 @dataclass
-class CommandClient:
-    registry: CommandRegistry
+class CommandTree:
+    parent: CommandTree | None
+    data: Command
+    children: list[CommandTree]
 
-    def __str__(self) -> str:
-        return "CommandClient"
+    def get_command_sequence(self):
+        sequence: list[CommandTree] = []
 
-    def register(self, command: Command):
-        self.registry[command.name] = command
+        curr = self
 
-        logging.info(f"registered command '{command.name}'")
+        in_root = False
 
-    def get_func(self, command: CommandNode):
-        command_seq = get_keyword_seq(command)
-
-        func = self.__get_func(self.registry, command_seq)
-
-        if func:
-            return func
-
-        raise Exception(f"cannot find implementation for '{' '.join(command_seq)}'")
-
-    def __get_func(self, registry: CommandRegistry, command_sequence: list[str]) -> t.Callable | None:
-        name = command_sequence.pop(0)
-
-        command = registry.get(name)
-
-        if command is not None:
-            if len(command_sequence) != 0:
-                return self.__get_func(command.registry, [*command_sequence])
+        while not in_root:
+            sequence.insert(0, curr)
+            if curr.parent is None:
+                in_root = True
 
             else:
-                return command.func
-        else:
-            raise Exception(f"could not find command '{name}'")
+                curr = curr.parent
+        return sequence
 
-    async def execute(self, command: CommandNode):
-        func = self.get_func(command)
+    def command(self, name: str) -> t.Callable[[t.Callable[..., t.Any | None]], CommandTree]:
+        def __wrap__(func: t.Callable[..., t.Any | None]):
+            children_names = list(map(lambda c: c.data.name, self.children))
 
-        return await func()
+            if name in children_names:
+                command_sequence = self.get_command_sequence()
 
-    def command(self, func: t.Callable[..., t.Any | None]) -> Command:
-        return Command(func.__name__, func, {})
+                full_command_path = list(map(lambda x: x.data.name, command_sequence))
+                full_command_path.append(name)
+                full_command_debug_name = " ".join(full_command_path)
+
+                raise Exception(f"cannot add duplicate command '{full_command_debug_name}'")
+
+            data = Command(name=name, func=func)
+            tree = CommandTree(parent=self, data=data, children=[])
+
+            self.children.append(tree)
+
+            return tree
+
+        return __wrap__
 
 
-def command(func: t.Callable[..., t.Any | None]) -> Command:
-    return Command(func.__name__, func, {})
+root = CommandTree(
+    parent=None,
+    children=[],
+    data=Command(name=".", func=lambda: print("hello!")),
+)
 
 
-# TODO
+@root.command(name="main")
+def main():  # pyright: ignore
+    return 0
+
+
+@main.command(name="submain1")
+def submain1():  # pyright: ignore
+    return "gg1a"
+
+
+@main.command(name="submain2")
+def submain2():
+    return "gg1b"
+
+
+@submain2.command(name="gg1")
+def gg1():
+    return "gg1b"
+
+
+@submain2.command(name="gg2")
+def gg2():
+    return "gg1b"
+
+
+@main.command(name="submain3")
+def submain3():
+    return "gg1b"
+
+
+@main.command(name="submain4")
+def submain4():
+    return "gg1b"
+
+
+# root.add_command(Command(name="hello", func=lambda: print("hello!")))
+
+
+pprint(root)
+
+
+for command in root.children[0].children[0].get_command_sequence():
+    print(command.data.name)
